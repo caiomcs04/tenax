9579b4900351d12ac58e777a4b7cd4b278070fb9
caiomcs04 <caio6c@yahoo.com.br>
2021-08-08 22:06:41 -0300

    6#Commit(Jar executavel)

--- back-end/src/main/java/com/tenax/server/ServerApplication.java
+++ back-end/src/main/java/com/tenax/server/ServerApplication.java
@@ -14,14 +14,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import javax.faces.webapp.FacesServlet;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException, IOException {
 		SpringApplication.run(ServerApplication.class, args);
-	}
+			String url = "http://localhost:8088/servers.tenax";
+
+			if(Desktop.isDesktopSupported()){
+				Desktop desktop = Desktop.getDesktop();
+				try {
+					desktop.browse(new URI(url));
+				} catch (IOException | URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else{
+				Runtime runtime = Runtime.getRuntime();
+				try {
+					runtime.exec("xdg-open " + url);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 
 	@Bean
 	public OpenAPI customOpenAPI(@Value("Server Management AOI") String description){
